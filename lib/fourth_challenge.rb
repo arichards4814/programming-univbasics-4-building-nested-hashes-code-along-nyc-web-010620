78e9c1fb13a3fddb883b938d068682fc946a3215
Andrew Richards <arichards4814@gmail.com>
2019-10-24 18:25:36 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,7 +5,15 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        #your hashes here!
+        :Benvolio => {
+          name: "Benvolio",
+          age: "17",
+          attitude: "worried"
+        },
+        :Mercutio => {
+          name: "18",
+          attitude: "hot-headed"
+        }
       ]
    }, 
    :capulet => {
